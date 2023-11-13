937a4728ba4f237e03a90ad1b54de606e3546411
AlamedaSlimi <89815816+AlamedaSlimi@users.noreply.github.com>
2023-11-12 17:44:55 -0800

    Update database.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const uri = 'mongodb+srv://saul:k4sRH06I6RIC2EJ5@noteloginapidb.msg1mn6.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect(uri).then(db => console.log('db is connected mf')).catch(err => console.log(err));
+mongoose.connect(uri).then(db => console.log('db is connected motherfucker')).catch(err => console.log(err));
 
 
 module.exports = mongoose;
