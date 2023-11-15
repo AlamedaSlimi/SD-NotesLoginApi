05adc985a271b4fecd0f7e716e22b15c635f2609
AlamedaSlimi <89815816+AlamedaSlimi@users.noreply.github.com>
2023-11-14 21:08:31 -0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
@@ -8,6 +8,32 @@ import CreateNote from './components/CreateNote';
 import CreateUser from './components/CreateUser';
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  // http://api.quotable.io/random
+
+  useEffect(() => {
+    fetch("http://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (quote) => {
+          setQuote(quote.content);  
+          setAuthor(quote.author);
+        }
+      )
+  },[]);
+
+  let fetchNewQuote = () => {
+    fetch("http://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (quote) => {
+          setQuote(quote.content);  
+          setAuthor(quote.author);
+        }
+      )
+  }
+  
   return (
     <Router>
     <Navigation/>
@@ -16,6 +42,13 @@ function App() {
       <Route path={"/edit/:id"} component={CreateNote} />
       <Route path="/create" component={CreateNote} />
       <Route path="/user" component={CreateUser} />
+      </div>
+      <div className="App">
+           <div className="quote">
+              <h2>{quote}</h2>
+              <small>-{author}-</small>
+           </div><br />
+           <button className="btn" onClick={fetchNewQuote}>Generate New Quote</button>
       </div>
     </Router>
   );
